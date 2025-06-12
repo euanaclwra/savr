e365ca2a47438240a877b4ea75ab284e7d132e86
Ana Clara <anaclarasilva82@gmail.com>
2025-06-12 17:58:14 -0300

    Ajustes Reunião

---
+++ __recovery/uCategoriaDAO.pas
@@ -0,0 +1,17 @@
+unit uCategoriaDAO;
+
+interface
+
+uses
+  uConfigGeral, uConexao, System.SysUtils, FireDAC.Comp.Client, FireDAC.Stan.Param,
+  FireDAC.DApt, uUtils;
+
+type
+  TCategoriaDAO = class
+  public
+    function InsereCategoria()
+  end;
+
+implementation
+
+end.
