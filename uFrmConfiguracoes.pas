9091bf54c7c9cc3d0e21b23754591b38a5a1e5ff
Ana Clara <anaclarasilva82@gmail.com>
2025-06-24 18:20:19 -0300

    Criação do Form de config. renda

--- uFrmConfiguracoes.pas
+++ uFrmConfiguracoes.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, uFrmConfigUsuario;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, uFrmConfigUsuario,
+  uFrmConfigRenda;
 
 type
   TfrmConfiguracoes = class(TForm)
@@ -22,6 +23,7 @@ type
     Line1: TLine;
     imgArrowRenda: TImage;
     procedure btnUsuarioClick(Sender: TObject);
+    procedure btnRendaClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -34,6 +36,11 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmConfiguracoes.btnRendaClick(Sender: TObject);
+begin
+  frmConfigRenda.Show;
+end;
 
 procedure TfrmConfiguracoes.btnUsuarioClick(Sender: TObject);
 begin
